df938d52a63f09a5d9790bd74ecf4bac56ffb243
Vahe1209 <Vahe1209@gmail.com>
2021-06-18 11:39:04 +0400

    add Search menu

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-let video = document.getElementById("myVideo");
+let searchMenu = document.getElementsByClassName("search-menu")[0];
+let filmSearch = document.getElementById("film-search");
+let search = document.getElementsByClassName("search")[0];
 
-let btn = document.getElementById("myBtn");
+search.addEventListener("click", () => {
+  searchMenu.classList.toggle("hide");
+});
 
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
+searchMenu.addEventListener("mouseover", (e) => {
+  searchMenu.classList.toggle("hide");
+});
+
+search.addEventListener("mouseout", (e) => {
+  searchMenu.classList.toggle("hide");
+});
