84488ba3a22360a8fc5e25b4c3df141820d99bc0
Edmon <you@example.com>
2021-06-21 18:03:18 +0400

    feat(add): email validation and button event

--- about.js
+++ about.js
@@ -1,11 +1,7 @@
-// let btn = document.getElementById("btn");
-// let hiddedDiv = document.querySelector('.wis')
-
-// btn.addEventListener("click", () => {
-//     hiddedDiv.classList.toggle('show')
-//     console.log(hiddedDiv.classList)
-// })
-
+const state = {
+  value: ""
+}
+let modal = document.querySelector(".modal")
 let wrapper = document.querySelector('.btn-container')
 let hiddedDiv = document.querySelector('.HidenText')
 wrapper.addEventListener('click', e => {
@@ -14,3 +10,42 @@ wrapper.addEventListener('click', e => {
         console.log(e.target.parentElement)
     }
 })
+console.log(modal.children.length);
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+let emailInput = document.querySelector(".Email_Reg")
+let buttonGetStarted = document.querySelector(".Get_Started_Btn")
+  buttonGetStarted.addEventListener("click", (e) => {
+      if(validateEmail(state.value) && (modal.children.length < 1)){ 
+        modal.style.display = "flex"
+        let p = document.createElement("p");
+        let text = document.createTextNode("Successful")
+        p.appendChild(text)
+        modal.appendChild(p)
+        setTimeout(()=> {
+          modal.style.display = "none"
+          modal.removeChild(p)
+        },2000)
+      }else if(!validateEmail(state.value) && (modal.children.length < 1)){
+        modal.style.display = "flex"
+        modal.style.backgroundColor = "red";
+        modal.style.borderColor = "red"
+        let p = document.createElement("p");
+        let text = document.createTextNode("Please enter a valid email.")
+        p.appendChild(text)
+        modal.appendChild(p)
+        setTimeout(()=> {
+          modal.style.display = "none"
+          modal.style.backgroundColor = "green";
+          modal.style.borderColor = "green"
+          modal.removeChild(p)
+          
+        },2000)
+      }
+
+  })
+  emailInput.addEventListener('change',(e) => {
+      state.value = e.target.value
+  })
