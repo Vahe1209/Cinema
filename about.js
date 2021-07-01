55da8984de79530adee0c15583570945cba16f24
Edmon <you@example.com>
2021-07-01 11:23:00 +0400

    feat(modal): add modal depends email

--- about.js
+++ about.js
@@ -10,7 +10,6 @@ wrapper.addEventListener('click', e => {
         console.log(e.target.parentElement)
     }
 })
-console.log(modal.children.length);
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
